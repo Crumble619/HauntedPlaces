ce024fef847fd5d9e81b9bb481320376e5715616
Colin Greene <crumble619@gmail.com>
2021-12-29 18:17:43 -0800

    Initial Commit

---
+++ HauntedPlaces/HauntedPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HauntedPlacesApp.swift
+//  HauntedPlaces
+//
+//  Created by Colin Greene on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct HauntedPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
